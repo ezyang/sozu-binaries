3e9cb9962550fdd8ced0e0a7093f915569c83f4a
Pushbot <pushbot@testing-gce-cf524b5d-b6d2-43a5-9f29-d8641b51deb5>
2017-02-03 05:58:57 +0000

    {"account":"ezyang", "repo":"cabal", "commit": "470e59a6bc5088ccac5996f3bd3a1f2a24a712ee", "tag":"linux-8.0.1"}

---
+++ dist-newstyle/build/cabal-install-1.25.0.0/setup/setup_macros.h
@@ -0,0 +1,28 @@
+/* package Cabal-1.25.0.0 */
+#define VERSION_Cabal "1.25.0.0"
+#define MIN_VERSION_Cabal(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  25 || \
+  (major1) == 1 && (major2) == 25 && (minor) <= 0)
+
+/* package base-4.9.0.0 */
+#define VERSION_base "4.9.0.0"
+#define MIN_VERSION_base(major1,major2,minor) (\
+  (major1) <  4 || \
+  (major1) == 4 && (major2) <  9 || \
+  (major1) == 4 && (major2) == 9 && (minor) <= 0)
+
+/* package filepath-1.4.1.0 */
+#define VERSION_filepath "1.4.1.0"
+#define MIN_VERSION_filepath(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  4 || \
+  (major1) == 1 && (major2) == 4 && (minor) <= 1)
+
+/* package process-1.4.2.0 */
+#define VERSION_process "1.4.2.0"
+#define MIN_VERSION_process(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  4 || \
+  (major1) == 1 && (major2) == 4 && (minor) <= 2)
+
