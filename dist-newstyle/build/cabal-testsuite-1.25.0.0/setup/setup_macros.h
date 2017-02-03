5541ef497dc482711aae7a734efb19c25e5fb2c9
Pushbot <pushbot@testing-gce-a60c1ab7-270c-462f-a08f-10e05848c39f>
2017-02-03 02:11:22 +0000

    {"account":"ezyang", "repo":"cabal", "commit": "f783de8e5b528eb36a60b71e71e00a56b1e76622", "tag":"linux-7.10.3"}

---
+++ dist-newstyle/build/cabal-testsuite-1.25.0.0/setup/setup_macros.h
@@ -0,0 +1,14 @@
+/* package Cabal-1.25.0.0 */
+#define VERSION_Cabal "1.25.0.0"
+#define MIN_VERSION_Cabal(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  25 || \
+  (major1) == 1 && (major2) == 25 && (minor) <= 0)
+
+/* package base-4.8.2.0 */
+#define VERSION_base "4.8.2.0"
+#define MIN_VERSION_base(major1,major2,minor) (\
+  (major1) <  4 || \
+  (major1) == 4 && (major2) <  8 || \
+  (major1) == 4 && (major2) == 8 && (minor) <= 2)
+
