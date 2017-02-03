8284d3477205266aaee1ee47a2025f683965618c
Pushbot <pushbot@testing-gce-a77e9f6c-d9c0-4ae6-8639-18bd0bcf51b7>
2017-02-03 04:53:47 +0000

    {"account":"ezyang", "repo":"cabal", "commit": "ab64fa7a1340f0277c4a9bd3cdd92f6d093218d0", "tag":"linux-7.10.3"}

---
+++ dist-newstyle/build/cabal-testsuite-1.25.0.0/build/cabal-tests/autogen/Paths_cabal_testsuite.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -fno-warn-missing-import-lists #-}
+{-# OPTIONS_GHC -fno-warn-implicit-prelude #-}
+module Paths_cabal_testsuite (
+    version,
+    getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+#if defined(VERSION_base)
+
+#if MIN_VERSION_base(4,0,0)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#else
+catchIO :: IO a -> (Exception.Exception -> IO a) -> IO a
+#endif
+
+#else
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+#endif
+catchIO = Exception.catch
+
+version :: Version
+version = Version [1,25,0,0] []
+bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/home/travis/.cabal/bin"
+libdir     = "/home/travis/.cabal/lib/x86_64-linux-ghc-7.10.3/.fake.cabal-testsuite-1.25.0.0"
+dynlibdir  = "/home/travis/.cabal/lib/x86_64-linux-ghc-7.10.3"
+datadir    = "/home/travis/.cabal/share/x86_64-linux-ghc-7.10.3/cabal-testsuite-1.25.0.0"
+libexecdir = "/home/travis/.cabal/libexec"
+sysconfdir = "/home/travis/.cabal/etc"
+
+getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "cabal_testsuite_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "cabal_testsuite_libdir") (\_ -> return libdir)
+getDynLibDir = catchIO (getEnv "cabal_testsuite_dynlibdir") (\_ -> return dynlibdir)
+getDataDir = catchIO (getEnv "cabal_testsuite_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "cabal_testsuite_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "cabal_testsuite_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
